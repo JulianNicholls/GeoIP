3becf676acd3ad417ea68077d27466ec4b8e55cc
Julian Nicholls <juliannicholls29@gmail.com>
2014-07-09 19:39:11 +0100

    num_start, num_end, and loc_ip are stored as int

--- ipranges.rb
+++ ipranges.rb
@@ -4,11 +4,23 @@ require 'veneer'
 
 # Veneer over Mongo for the locations collection
 class IPRanges < MongoVeneer
-  @fields = %w(num_start, num_end, loc_id, ip_start, ip_end)
+  @fields = %w(num_start num_end loc_id ip_start ip_end)
 
   def initialize
     super
 
     @coll = @db['ipranges']
+  end
+
+  protected
+
+  def record( row )
+    record = super
+
+    record['num_start'] = record['num_start'].to_i
+    record['num_end'] = record['num_end'].to_i
+    record['loc_id'] = record['loc_id'].to_i
+
+    record
   end
 end
