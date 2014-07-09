578f089c7489ec6fce04505644eecb8601421605
Julian Nicholls <juliannicholls29@gmail.com>
2014-07-09 19:43:24 +0100

    Added IP checking functionality.

--- manage_geoip.rb
+++ manage_geoip.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby -I.
 
+require 'pp'
+
 require 'locations'
 require 'ipranges'
 require 'ipholder'
@@ -26,12 +28,12 @@ class GeoIPManager
       print menu
       option = gets.chomp
 
-      case option.to_i
-      when 4 then break
+      case option[0].downcase
+      when 'x' then break
 
-      when 1 then load_locations
-      when 2 then load_ips
-      when 3 then check_ip
+      when 'l' then load_locations
+      when 'i' then load_ips
+      when 'c' then check_ip
       end
     end
   end
@@ -39,8 +41,8 @@ class GeoIPManager
   private
 
   def show_record_counts
+    printf "\nIP Ranges: %9s\n", @ips.count.with_commas
     printf "Locations: %9s\n", @loc.count.with_commas
-    printf "IP Ranges: %9s\n", @ips.count.with_commas
   end
 
   def load_locations
@@ -53,15 +55,74 @@ class GeoIPManager
     @ips.build_from_csv( 'ips.csv' )
   end
 
+  def check_ip
+    print "\nEnter IP as x.x.x.x or Numeric: "
+    response = gets.chomp
+    ip = IPHolder.new( response )
+    puts "\nChecking #{ip}"
+
+    crit = { num_start: {'$lte' => ip.numeric}, num_end: {'$gte' => ip.numeric} }
+    ips  = @ips.find( crit ).to_a
+
+    case ips.size
+    when 0  then puts "No IP Range found"
+
+    when 1  then
+      addr = ips[0]
+      puts ip_data( addr )
+
+      locs = @loc.find( { loc_id: addr['loc_id'] } ).to_a
+
+      if locs.size > 0
+        puts loc_data( locs[0] )
+      else
+        puts "No Location Found"
+      end
+    else
+      puts "WEIRD!"
+      pp ips
+    end
+  end
+
   def menu
     %{
-(1) Load Locations.
-(2) Load IPs.
-(3) Check an IP.
+  (L)oad Locations.
+  Load (I)Ps.
+  (C)heck an IP.
 
-(4) Exit
+  E(x)it
 
-  Select: }
+Select: }
+  end
+
+  def ip_data( ip )
+    num_s, num_e = ip['num_start'], ip['num_end']
+    %{
+Range:    #{num_s} - #{num_e} (#{(num_e - num_s + 1).with_commas} Addressses)
+IP:       #{ip['ip_start']} - #{ip['ip_end']}
+Location: #{ip['loc_id']}
+    }
+  end
+
+  def loc_data( loc )
+    # pp loc
+
+    region    = loc.fetch( 'region', '' ).empty? ? '' : ", Region: #{loc['region']}"
+    city      = loc.fetch( 'city', '' ).empty? ? '' : ", City: #{loc['city']}"
+    postcode  = loc.fetch( 'postcode', '' ).empty? ? '' : "Postcode: #{loc['postcode']}"
+    metrocode = loc.fetch( 'metrocode', '' ).empty? ? '' : ", Metro Code: #{loc['metrocode']}"
+    areacode  = loc.fetch( 'areacode', '' ).empty? ? '' : ", Area Code: #{loc['areacode']}"
+
+    %{
+ID:       #{loc['loc_id']}
+Lat Long: #{lat_long loc['lat_long']}
+Location  Country: #{loc['country']}#{region}#{city}
+Postal:   #{postcode}#{metrocode}#{areacode}
+    }
+  end
+
+  def lat_long( ll )
+    "#{ll[0].abs}#{ll[0] > 0 ? 'N' : 'S'}, #{ll[1].abs}#{ll[1] > 0 ? 'E' : 'W'}"
   end
 end
 
