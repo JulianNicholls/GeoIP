6bfe1074dfcd5aaf28267d9f017e5d5c49c1902c
Julian Nicholls <juliannicholls29@gmail.com>
2014-07-09 19:40:01 +0100

    Record is updated before saving.
    Empty fields are removed.
    Lat and Long are put into an array.
    loc_id is stored as an int.

--- locations.rb
+++ locations.rb
@@ -4,11 +4,34 @@ require 'veneer'
 
 # Veneer over Mongo for the locations collection
 class Locations < MongoVeneer
-  @fields = %w(loc_id country region city postcode latitude longitude metrocode areacode)
+  @fields = %w(loc_id country region city postcode lat long metrocode areacode)
 
   def initialize
     super
 
     @coll = @db['locations']
+  end
+
+  protected
+
+  def record( row )
+    @record = super
+
+    @record['loc_id'] = @record['loc_id'].to_i
+
+    convert_lat_long
+
+    @record.delete_if { |k, v| v.nil? }
+
+    @record
+  end
+
+  private
+
+  def convert_lat_long
+    @record['lat_long'] = [@record['lat'].to_f, @record['long'].to_f]
+
+    @record.delete 'lat'
+    @record.delete 'long'
   end
 end
