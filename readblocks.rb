a028494a28477e4eee0516e46fd74b4fab8c4f3f
Julian Nicholls <juliannicholls29@gmail.com>
2014-07-11 16:32:29 +0100

    Updated to use ipholder

--- readblocks.rb
+++ readblocks.rb
@@ -1,16 +1,17 @@
 #!/usr/bin/env ruby -I.
 
 require 'csv'
-require 'pp'
 
-require 'iptool'
+require 'ipholder'
 
+# Converter of IP block CSV ile so that the dotted addresses are included
 class BlocksReader
   def initialize( filename = 'GeoLiteCity-Blocks.csv' )
     output = CSV.open( 'output.csv', 'wb' )
 
     CSV.foreach( filename ) do |row|
       next unless row.size > 1
+
       ip_start = IPConverter.new( row[0] )
       ip_end   = IPConverter.new( row[1] )
       row.push( ip_start.ip, ip_end.ip )
